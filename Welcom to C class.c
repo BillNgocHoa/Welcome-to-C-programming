0f90c9df2c50b7e6250803a45dcccc19e51b764a
Bill Ngoc Hoa <115616583+BillNgocHoa@users.noreply.github.com>
2023-04-10 14:44:41 +0700

    Welcom to C
    This is a simple C program that prints out two lines of text using the printf() function. It first prints "Welcom to the C class" and then "My name is Hoa". The program returns 0 to indicate successful execution.

---
+++ Welcom to C class.c
@@ -0,0 +1,12 @@
+// The very first program
+// Nguyễn Ngọc Hòa _ ITITUN21010
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Welcom to the C class\n");
+    printf("My name is Hoa\n");
+
+    return 0;
+}
